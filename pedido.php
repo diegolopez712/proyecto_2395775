8611886cb0ab9b7e2d4708cbcf745b40ea8d8186
diegolopez712 <90726954+diegolopez712@users.noreply.github.com>
2022-05-24 15:14:36 -0500

    Add files via upload

---
+++ pedido.php
@@ -0,0 +1,80 @@
+<?php
+require_once('conexion.php');
+class Pedido{
+    private $idPedido;
+    private $idCliente;
+    private $fecha;
+    private $estado;
+    private $idUsuario;
+    
+
+    public function __construct(){
+
+    }
+
+    public function setidPedido($idPedido){
+        $this->idPedido = $idPedido;
+    }
+
+    public function setidCliente($idCliente){
+        $this->idCliente = $idCliente;
+    }
+
+    public function setfecha($fecha){
+        $this->fecha = $fecha;
+    }
+
+    public function setestado($estado){
+        $this->estado = $estado;
+    }
+
+    public function setidUsuario($idUsuario){
+        $this->idUsuario = $idUsuario;
+    }
+
+    public function getidPedido(){
+        return $this->idPedido;
+    }
+
+    public function getidCliente(){
+        return $this->idCliente;
+    }
+
+    public function getfecha(){
+        return $this->fecha;
+    }
+
+    public function getestado(){
+        return $this->estado;
+    }
+
+    public function getidUsuario(){
+        return $this->idUsuario;
+    }
+
+    public function registrarPedido($pedido){
+        $mensaje = "";
+        //Establecer la conexión a la base datos
+        $baseDatos = Conexion::conectar();
+        //Preparar la sentencia sql
+        $sql = $baseDatos->prepare('INSERT INTO 
+        pedido(idPedido,idCliente, fecha, estado, idUsuario)
+        VALUES(:idPedido,:idCliente, now(), :estado,:idUsuario) ');
+        $sql->bindValue('idPedido', '');
+        $sql->bindValue('idCliente', $factura->getidCliente());
+        //$sql->bindValue('fecha', $producto->getnombre());
+        $sql->bindValue('estado', $factura->getestado());
+        $sql->bindValue('idUsuario', $factura->getidUsuario());
+        try{
+            $sql->execute(); //Ejecutar el sql
+            $mensaje = $baseDatos->lastInsertId();
+        }
+        catch(Excepcion $e){
+            $mensaje = $e->getMessage(); //Obtener el mensaje de error.
+        }
+        Conexion::desconectar($baseDatos); //Cierra la conexión.
+        return $mensaje;
+    }
+}
+
+?>
