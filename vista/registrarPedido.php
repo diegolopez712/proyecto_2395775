7b4ff30ee1edd294bcf3c5540926301f75a2869b
diegolopez712 <90726954+diegolopez712@users.noreply.github.com>
2022-05-24 17:39:45 -0500

    Add files via upload

--- vista/registrarPedido.php
+++ vista/registrarPedido.php
@@ -1,4 +1,8 @@
 <?php
+require_once('../controlador/controladorProducto.php');
+$controladorProducto = new controladorProducto();
+$listarProducto = $controladorProducto->listarProducto();
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,20 +15,52 @@
 <body>
     <form action="" method="post">
         <label>Id Pedido</label>
-        <input type="text" id="idPedido" name="idPedido" />
+        <input readonly type="text" id="idPedido" name="idPedido" />
         <br>
         <label>Cliente</label>
         <input type="text" name="idCliente" id="idCliente" />
         <br>
+        <label>Producto:</label>
+        <select name="idProducto" id="idProducto">
+        <?php
+            foreach($listarProducto as $producto){
+        ?>
+            <option value="<?php echo $producto['idProducto'] ?>">
+                <?php echo $producto['nombre'] ?>
+            </option>
+        <?php
+            }
+        ?>
+        </select>
+        <br>
+        <label>Precio</label>
+        <input type="text" id="precio" name="precio" onkeypress="calcularSubtotal()" onkeydown="calcularSubtotal()" onkeyup="calcularSubtotal()" onblur="calcularSubtotal()" />
+        <br>
+        <label>Cantidad</label>
+        <input type="text" id="cantidad" name="cantidad" onkeypress="calcularSubtotal()" onkeydown="calcularSubtotal()" onkeyup="calcularSubtotal()" onblur="calcularSubtotal()" />
+        <br>
+        <label>SubTotal:</label>
+        <input readonly type="text" id="subTotal" name="subTotal" />
+        <br>
         <input type="hidden" name="accion" value="Registrar" />
-        <input type="button" onclick="registrar()" />
+        <button type="button" onclick="registrar()">Agregar</button>
     </form>
 
     <script>
+        function calcularSubtotal(){
+            let precio = document.getElementById('precio').value;
+            let cantidad = document.getElementById('cantidad').value;
+            document.getElementById('subTotal').value = precio * cantidad;
+        }
+
          async function registrar() {
 
             let formData = new FormData();
             formData.append("idPedido", document.getElementById('idPedido').value);
+            formData.append("idCliente", document.getElementById('idCliente').value);
+            formData.append("idProducto", document.getElementById('idProducto').value);
+            formData.append("cantidad", document.getElementById('cantidad').value);
+            formData.append("precio", document.getElementById('precio').value);
             formData.append("accion", 'Registrar');
             //formData.append("image", imageBlob, "image.png");
 
@@ -34,7 +70,7 @@
             });
             let result = await response.text();
             document.getElementById('idPedido').value = result;
-            alert(result);
+            //alert(result);
         }
 
     </script>
